1b181fa4b115dc5585592bde10d2c95e90549fb5
Johan Holmberg <johan@idioti.se>
2019-10-08 22:35:29 +0200

    Added a simple object storage

---
+++ src/controllers/Storage.py
@@ -0,0 +1,40 @@
+from entities.Serializable import Serializable
+
+class Storage:
+    '''
+    Storage represents a file storage that stores and retrieves objects
+    '''
+    
+    def __init__(self):
+        pass
+    
+    def save(self, filename: str, data: Serializable) -> bool:
+        '''
+        Stores a serializable object. If the object isn't explicitly marked as
+        being serializable, this method will fail.
+        '''
+        if not issubclass(data.__class__, Serializable):
+            return False
+        
+        f = open(filename, "w")
+        f.write(data.serialize())
+        f.close()
+        
+        return True
+    
+    def read(self, filename: str, class_name: type) -> Serializable:
+        '''
+        Retrieves a serialized object. You specify the type of he object to
+        deserialize by passing the class (as a type, not a string) as the
+        second parameter.
+        '''
+        if not issubclass(class_name, Serializable):
+            return None
+        
+        f = open(filename, "r")
+        data = f.read()
+        f.close()
+        
+        deserialized = class_name.deserialize(data)
+        
+        return deserialized
